be9fc33d0415e42ddda52cdf8fff89f34d4fc9bf
joykangangi <64706463+joykangangi@users.noreply.github.com>
2023-05-24 23:30:31 +0300

    SetUp Search Screen

--- app/src/main/java/com/jkangangi/en_dictionary/home/SearchScreen.kt
+++ app/src/main/java/com/jkangangi/en_dictionary/home/SearchScreen.kt
@@ -1,24 +1,94 @@
 package com.jkangangi.en_dictionary.home
 
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.WbSunny
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jkangangi.en_dictionary.R
+import com.jkangangi.en_dictionary.ui.theme.En_DictionaryTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Search(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
+    isDarkTheme: Boolean,
+    toggleTheme: ()-> Unit
 ) {
+
     Scaffold(
         modifier = modifier,
         topBar = {
-            MediumTopAppBar(actions = ) {
-                
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home_text)) },
+                actions = {
+                    IconButton(onClick = toggleTheme) {
+                        Icon(
+                            imageVector = if (isDarkTheme) Icons.Default.DarkMode else Icons.Default.WbSunny,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                },
+                modifier = modifier.shadow(elevation = 2.dp),
+            )
+        },
+        content = { contentPadding ->
+            Column(
+                modifier = modifier
+                    .padding(contentPadding)
+                    .padding(start = 20.dp, top = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dictionary),
+                    contentDescription = null,
+                    modifier = modifier.size(200.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.home_body),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                OutlinedTextField(
+                    value = "",//Todo VM
+                    onValueChange = { },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null
+                        )
+                    },
+                )
             }
-        }
+        },
+    )
+}
 
-    ) {
-        
+@Preview
+@Composable
+fun HomePreview() {
+    En_DictionaryTheme {
+        Search(isDarkTheme = false, toggleTheme = { })
     }
 }
