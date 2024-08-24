3959b8673f541e84450915fb73030053eded3cdd
joykangangi <64706463+joykangangi@users.noreply.github.com>
2024-08-24 16:51:23 +0300

    fix:appyx NodeComponentActivity is crashing

--- app/src/main/java/com/jkangangi/en_dictionary/MainActivity.kt
+++ app/src/main/java/com/jkangangi/en_dictionary/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.jkangangi.en_dictionary
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bumble.appyx.core.integration.NodeHost
-import com.bumble.appyx.core.integrationpoint.NodeComponentActivity
+import com.bumble.appyx.core.integrationpoint.ActivityIntegrationPoint
+import com.bumble.appyx.core.integrationpoint.IntegrationPointProvider
 import com.jkangangi.en_dictionary.app.navigation.Navigation
 import com.jkangangi.en_dictionary.app.theme.En_DictionaryTheme
 import com.jkangangi.en_dictionary.settings.SettingsViewModel
 
 
-class MainActivity : NodeComponentActivity() {
+/**
+ * Root Activity for the app
+ *
+ * Using NodeComponentActivity causes a build error:
+ *  -Inherited platform declarations clash: The following declarations have the same JVM signature (onRequestPermissionsResult(I[Ljava/lang/String;[I)V):
+ *     fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Unit defined in appyx.MainActivity
+ *     fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray): Unit defined in .appyx.MainActivity
+ *
+ * Inheriting [IntegrationPointProvider] manually seems to help.
+ */
+
+class MainActivity : ComponentActivity(), IntegrationPointProvider {
+
+    override lateinit var appyxV1IntegrationPoint: ActivityIntegrationPoint
+
+    private fun createIntegrationPoint(savedInstanceState: Bundle?) =
+        ActivityIntegrationPoint(
+            activity = this,
+            savedInstanceState = savedInstanceState
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appyxV1IntegrationPoint = createIntegrationPoint(savedInstanceState)
 
         setContent {
 
@@ -27,7 +49,7 @@ class MainActivity : NodeComponentActivity() {
                 fontFamily = settings.font.fontFamily,
                 content = {
                     NodeHost(
-                        integrationPoint = appyxIntegrationPoint,
+                        integrationPoint = appyxV1IntegrationPoint,
                         factory = { context ->
                             Navigation(
                                 rootBuildContext = context
@@ -38,4 +60,37 @@ class MainActivity : NodeComponentActivity() {
             )
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        appyxV1IntegrationPoint.onSaveInstanceState(outState)
+    }
 }
+
+//class MainActivity : NodeComponentActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        setContent {
+//
+//            val settingsViewModel: SettingsViewModel = viewModel(factory = SettingsViewModel.Factory)
+//            val settings by settingsViewModel.settingsState.collectAsState()
+//
+//            En_DictionaryTheme(
+//                darkTheme = settings.darkTheme,
+//                fontFamily = settings.font.fontFamily,
+//                content = {
+//                    NodeHost(
+//                        integrationPoint = appyxV1IntegrationPoint,
+//                        factory = { context ->
+//                            Navigation(
+//                                rootBuildContext = context
+//                            )
+//                        },
+//                    )
+//                }
+//            )
+//        }
+//    }
+//}
