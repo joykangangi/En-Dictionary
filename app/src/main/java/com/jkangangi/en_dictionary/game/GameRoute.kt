dafbfb9e60490e932bd65a826a4e0cf845fa5127
Joy Kangangi <64706463+joykangangi@users.noreply.github.com>
2025-01-19 22:35:27 +0300

    Update GameRoute.kt

--- app/src/main/java/com/jkangangi/en_dictionary/game/GameRoute.kt
+++ app/src/main/java/com/jkangangi/en_dictionary/game/GameRoute.kt
@@ -1,6 +1,6 @@
 package com.jkangangi.en_dictionary.game
 
-import com.jkangangi.en_dictionary.game.mode.model.GameModeParam
+import com.jkangangi.en_dictionary.game.mode.model.GameMode
 import com.jkangangi.en_dictionary.game.mode.model.GameSummaryStats
 import kotlinx.serialization.Serializable
 
@@ -15,16 +15,7 @@ sealed class GameRoute {
     data object GameIntroRoute : GameRoute()
 
     @Serializable
-    data class GameModeRoute(val gameMode: GameModeParam) : GameRoute()
-
-    @Serializable
-    data object EasyGameModeRoute : GameRoute()
-
-    @Serializable
-    data object MediumGameModeRoute : GameRoute()
-
-    @Serializable
-    data object HardGameModeRoute : GameRoute()
+    data class GameModeRoute(val gameMode: GameMode) : GameRoute()
 
     @Serializable
     data class GameSummaryDialogRoute(val gameSummaryStats: GameSummaryStats) : GameRoute()
